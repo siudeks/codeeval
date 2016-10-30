e051a90c80cc592249bc8f291b5eecba7b14618a
siudeks <siudeks@gmail.com>
2016-10-30 20:54:37 +0100

    Logic moved to pattern matching

--- src/main/scala/net/siudek/FizzBuzz.scala
+++ src/main/scala/net/siudek/FizzBuzz.scala
@@ -1,4 +1,3 @@
-
 // CodeEval solution:
 object Main extends App {
   import net.siudek._
@@ -13,15 +12,24 @@ object Main extends App {
 package net.siudek {
 
   object FizzBuzz {
+
     def produce(line: String): String = {
       val Array(a, b, c) = line split "\\s+" map (_.toInt)
       produce(a, b, c)
     }
 
     def produce(fizzNumber: Int, buzzNumber: Int, limit: Int): String = {
-      def asChar(x: Int) = if (x % fizzNumber == 0 && x % buzzNumber == 0) "FB" else if (x % fizzNumber == 0) "F" else if (x % buzzNumber == 0) "B" else x.toString
+      def asChar(x: Int) = (x, fizzNumber, buzzNumber) match {
+        case Dividable(true, true) => "FB"
+        case Dividable(true, false) => "F"
+        case Dividable(false, true) => "B"
+        case _ => `x` toString
+      }
       Stream.from(1).take(limit - 1).foldLeft("")((acc, cur) => acc + asChar(cur) + " ") + asChar(limit)
     }
-  }
 
+    object Dividable {
+      def unapply(tuple: (Int, Int, Int)) = Some((tuple._1 % tuple._2 == 0, tuple._1 % tuple._3 == 0))
+    }
+  }
 }
